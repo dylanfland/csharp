d4f546a5c030f7f31f14465edfcf635f54e9d016
Dylan Flandrin <dylan.flandrin@bearstudio.fr>
2023-04-17 11:36:29 +0200

    feat(calc-salary) : calc net salary with hard-coded values

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello World");
+﻿decimal salaireBrut = 30000;
+decimal impotSurLeRevenu = 0.30m;
+
+decimal salaireNet = salaireBrut - (salaireBrut * impotSurLeRevenu);
+
+Console.WriteLine("Le salaire net est de : " + salaireNet / 12 + " euros");
