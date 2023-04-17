ce574db5b02d3837d9eca7076df68c527f1aaacc
Dylan Flandrin <dylan.flandrin@bearstudio.fr>
2023-04-17 11:29:25 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+int salaireBrut = Int32.Parse(args[0]);
+Console.WriteLine(salaireBrut);
+decimal impotSurLeRevenu = Decimal.Parse(args[1]);
+
+decimal salaireNet = salaireBrut - (salaireBrut * impotSurLeRevenu);
+
+Console.WriteLine("Le salaire net est de : " + salaireNet / 12 + " euros");
